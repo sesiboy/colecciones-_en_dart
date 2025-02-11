0a461a97992f66150f071bb41b101999b4a32eca
jireh correa <166634719+sesiboy@users.noreply.github.com>
2025-02-11 16:54:05 +0000

    hola lista

---
+++ list1.dart
@@ -0,0 +1,12 @@
+void main() {  
+    List<int> numeros = [10,20,30];   
+   print("listas de numeros enteros:");
+   print(numeros);
+   print("primer elemento: ${numeros[0]}");
+   print("segundo elemento: ${numeros[1]}");
+   print("tercero elemento: ${numeros[2]}");
+   print("elementos de la lista usando ciclo for:");
+   for(int i =0; i < numeros.length; i++){
+    print(numeros[i]);
+   }
+}  
